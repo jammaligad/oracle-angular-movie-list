845f03474323b5f5dc589db87a1c2e8a1ca5cc05
Jam Maligad <jdmaligad@gmail.com>
2023-09-24 14:59:38 +0800

    Initialize Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Feature Modules
+import { MovieListComponent } from './features/movie-list/movie-list.component';
+
+// Components
+import { ButtonComponent } from './components/button/button.component';
+import { YearInputComponent } from './components/year-input/year-input.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    ButtonComponent,
+    YearInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/year-input/year-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'year-input',
+  templateUrl: './year-input.component.html',
+  styleUrls: ['./year-input.component.scss']
+})
+export class YearInputComponent {
+
+}
